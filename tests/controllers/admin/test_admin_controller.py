33193f914478208285b36885d2396102f95c34f2
nickolasvm <nickolas64vm@gmail.com>
2023-08-15 18:57:01 -0300

    test: Created tests for history_delete fucntion

--- tests/controllers/admin/test_admin_controller.py
+++ tests/controllers/admin/test_admin_controller.py
@@ -1,6 +1,32 @@
-# from src.models.history_model import HistoryModel
-# from src.models.user_model import UserModel
+import json
+from src.models.history_model import HistoryModel
+from src.models.user_model import UserModel
+
+user_mock = {"name": "um nome", "level": "admin", "token": "um token"}
+history_mock_1 = {
+    "text-to-translate": "ola me chamo Ana",
+    "translate-from": "pt",
+    "translate-to": "en",
+}
+history_mock_2 = {
+    "text-to-translate": "hello my name is Jhon",
+    "translate-from": "en",
+    "translate-to": "pt",
+}
 
 
 def test_history_delete(app_test):
-    raise NotImplementedError
+    UserModel(user_mock).save()
+    HistoryModel(history_mock_1).save()
+    HistoryModel(history_mock_2).save()
+
+    id = json.loads(HistoryModel.list_as_json())[0]["_id"]
+    app_test.delete(
+        f"/admin/history/{id}",
+        headers={
+            "Authorization": "um token",
+            "User": "um nome",
+        },
+    )
+
+    assert len(json.loads(HistoryModel.list_as_json())) == 1
