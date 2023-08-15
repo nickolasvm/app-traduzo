976c9b9359d320aed5e46efae042763f66042ab9
nickolasvm <nickolas64vm@gmail.com>
2023-08-15 16:34:14 -0300

    refactor: Refactored to dynamically change the trasnlate_from and translate_to variables

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -8,26 +8,32 @@ from models.language_model import LanguageModel
 translate_controller = Blueprint("translate_controller", __name__)
 
 
-# Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
-    languages = LanguageModel.list_dicts()
-
     if request.method == "POST":
-        source = request.form.get("translate-from")
-        target = request.form.get("translate-to")
         text_to_translate = request.form.get("text-to-translate")
-        translated = GoogleTranslator(source, target).translate(
+        translate_from = request.form.get("translate-from")
+        translate_to = request.form.get("translate-to")
+        translated = GoogleTranslator(translate_from, translate_to).translate(
             text_to_translate
         )
 
         return render_template(
-            "index.html", languages=languages, translated=translated
+            "index.html",
+            languages=LanguageModel.list_dicts(),
+            text_to_translate=text_to_translate,
+            translate_from=translate_from,
+            translate_to=translate_to,
+            translated=translated,
         )
     else:
         return render_template(
             "index.html",
-            languages=languages,
+            languages=LanguageModel.list_dicts(),
+            text_to_translate="",
+            translate_from="pt",
+            translate_to="en",
+            translated="Tradução",
         )
 
 
